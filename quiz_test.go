b160d2c783fda3ce0100823727de202b38708e34
Bob Campbell <bcampbell@pryor.com>
2017-03-07 12:32:36 -0600

    does not seem to want to behave as expected; keep getting "expected exression" error when try to go fmt

--- quiz_test.go
+++ quiz_test.go
@@ -7,6 +7,54 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Quiz", func() {
+// var _ = Describe("Quiz", func() {
+//     var quiz Quiz
 
-})
+//     BeforeEach(func(){
+
+//         answers := [1]string
+//         answers[0] = "yes"
+
+//         testQuestion := Question {
+//             Id: 100,
+//             Text: "Is this the first question?",
+//             Answers: answers,
+//             AdditionalInfo: "If there were further information, I would surely tell you."
+//         }
+
+//         questions:=[1] Question
+//         questions[0]=testQuestion
+
+//         quiz:=Quiz{
+//             About: "This is the quiz",
+//             Data: Data{
+//                 Questions: questions
+//             }
+//         }
+//     }
+// }
+// )
+
+//     var quiz quiz
+
+//     BeforeEach(func(){
+
+//         var answers [1]string
+//         answers[0]="yes"
+
+//         testQuestion := Question{
+// Id: 100,
+//                     Text: "Is this the first question?",
+//                     Answers: answers,
+//                     AdditionalInfo: "If there were further information, I would surely tell you.",
+//                     }
+
+//         var questions [1]question
+//         questions[0]=testQuestion
+
+//         quiz= Quiz{
+//             About: "This is the quiz",
+//             Data: Data{
+//                 Questions: questions,
+//         })
+// })
