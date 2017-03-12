ad8c0b532133266b2eb76af4144dbc372bda8411
Bob Campbell <mostlybob@gmail.com>
2017-03-12 07:49:47 -0500

    second test on Question passing - yay!

--- quiz_test.go
+++ quiz_test.go
@@ -30,7 +30,11 @@ var _ = Describe("Quiz", func() {
 
 	Describe("Questions are part of a Quiz", func() {
 		Context("A Question has some attributes", func() {
-			question := quiz.Data.Questions[0]
+			var question Question
+			BeforeEach(func() {
+
+				question = quiz.Data.Questions[0]
+			})
 			It("An id", func() {
 				Expect(question.Id).ToNot(Equal(0))
 			})
