e7d257634e374e7dc93f3e599ff6e40129eb194b
Bob Campbell <mostlybob@gmail.com>
2017-03-12 12:08:03 -0500

    test passing for AdditionalInfo

--- quiz_test.go
+++ quiz_test.go
@@ -18,9 +18,10 @@ var _ = Describe("Quiz", func() {
 	BeforeEach(func() {
 		quiz = Quiz{}
 		quiz.Data.Questions = append(quiz.Data.Questions, Question{
-			Id:      1,
-			Text:    "Here's some text?",
-			Answers: []string{"aaaaa"},
+			Id:             1,
+			Text:           "Here's some text?",
+			Answers:        []string{"aaaaa"},
+			AdditionalInfo: "some additional information",
 		})
 	})
 
