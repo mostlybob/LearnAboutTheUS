76d1f04fd7e278810ab053cf2d228dd28195d30e
Bob Campbell <mostlybob@gmail.com>
2017-04-02 19:10:12 -0500

    just testing to see that the code works from this subdirectoryf

---
+++ nfsn/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "github.com/mostlybob/LearnAboutTheUS/quiz"
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	learnUs := quiz.CreateQuizFromJSON(GetQuizJson("../LearnAboutTheUS.json"))
+
+	ShowAQuestion(learnUs)
+}
+
+func ShowAQuestion(quiz quiz.Quiz) {
+	question := quiz.GetRandomQuestion()
+	fmt.Printf("%d - %v\n", question.Id, question.Text)
+	fmt.Println("(press Enter to show answers)")
+
+	var input string
+	fmt.Scanln(&input)
+
+	for _, answer := range question.Answers {
+		fmt.Println(answer)
+	}
+}
+
+func GetQuizJson(pathToJson string) string {
+	// read the whole file at once - it's not that big
+	data, err := ioutil.ReadFile(pathToJson)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(data)
+}
