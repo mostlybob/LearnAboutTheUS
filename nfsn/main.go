8d84caef7be26b8bc2b231767ada6ca58637d010
Bob Campbell <bcampbell@pryor.com>
2017-04-03 12:22:28 -0500

    add the method to get the json

--- nfsn/main.go
+++ nfsn/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func getAQuestion(w http.ResponseWriter, r *http.Request) {
-	learnUs := quiz.CreateQuizFromJSON(GetQuizJson("../LearnAboutTheUS.json"))
+	learnUs := quiz.CreateQuizFromJSON(getQuizJson("../LearnAboutTheUS.json"))
 	question := showAQuestion(learnUs)
 	// should probably look at getting this as a json object, since that's what
 	// I want to return from the end point. Simple string for now.
@@ -54,12 +54,12 @@ func showAQuestion(quiz quiz.Quiz) string {
 	// }
 }
 
-// func GetQuizJson(pathToJson string) string {
-// 	// read the whole file at once - it's not that big
-// 	data, err := ioutil.ReadFile(pathToJson)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+func getQuizJson(pathToJson string) string {
+	// read the whole file at once - it's not that big
+	data, err := ioutil.ReadFile(pathToJson)
+	if err != nil {
+		panic(err)
+	}
 
-// 	return string(data)
-// }
+	return string(data)
+}
